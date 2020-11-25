b332a7362cbc51f2d1ae31da5281c7c9ae9e43b5
Abdullah Saad <asaad02@uoguelph.ca>
2020-11-25 01:05:52 -0500

    first version of code

---
+++ count_nodes.c
@@ -0,0 +1,35 @@
+/**
+ * @file count_nodes.c
+ * @author Abdullah Saad | 1043850 | asaad02@uoguelph.ca
+ * @date november 23th 2020
+ * @brief File c compiled to create a program called count_nodes.
+ **/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ttt.h"
+
+int main(int argc, char const *argv[])
+{
+     //call init_boards
+    init_boards();
+    // call  init_board(START_BOARD)
+    init_board(START_BOARD);
+    // call join_graph(START_BOARD) 
+    join_graph(START_BOARD);
+
+    int nodes = 0;
+
+    for (int i = 0; i < HSIZE; i++)
+    {
+        if (htable[i].init == 1)
+        {
+            nodes++;
+        }
+        
+    }
+    printf(" The number of entries in the htable %d\n ",nodes );
+    
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,31 @@
+CFLAGS = -std=c99 -Wall -pedantic   
+CC =gcc
+
+
+all: ttt2.o show_node count_nodes
+
+
+
+Clean:
+
+	rm* .o -i
+show_node: show_node.o ttt2.o ttt1.o
+	gcc -std=c99 -Wall -pedantic  ttt2.c ttt1.c show_node.c -o show_node
+
+count_nodes: count_nodes.o ttt2.o ttt1.o
+	gcc -std=c99 -Wall -pedantic   ttt2.c ttt1.c count_nodes.c -o count_nodes
+
+
+ttt1.o: ttt1.c
+	$(CC) $(CFLAGS) -c ttt1.c -o $@
+
+ttt2.o: ttt2.c
+	$(CC) $(CFLAGS) -c ttt2.c -o $@
+
+
+show_node.o:show_node.c
+	$(CC) $(CFLAGS) -c show_node.c -o $@
+
+count_nodes.o: count_nodes.c
+	$(CC) $(CFLAGS) -c  count_nodes.c  -o $@
+
